e5e3b342d9f984e07163a8460aa1744587b59d88
Bela Soares de Almeida Dutta <bela@rackham4.uppmax.uu.se>
2024-05-19 02:02:50 +0200

    sbatch script for FastQC after trimming (DNA)

---
+++ analyses/01_preprocessing/DNA_qc/DNA_fastqc_trim/trimmed_fastqc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -A uppmax2024-2-7 -M snowy
+#SBATCH -p core                   
+#SBATCH -n 6
+#SBATCH -t 00:45:00                
+#SBATCH -J trimmed_fastqc         
+#SBATCH --mem=6G                     
+#SBATCH -o %j.out       
+#SBATCH -e %j.err
+
+module load bioinfo-tools
+module load FastQC/0.11.9
+
+INPUT_DIR=/home/bela/genome_analysis/analyses/01_preprocessing/trimming
+OUTPUT_DIR=/home/bela/genome_analysis/analyses/01_preprocessing/fastqc_trim
+
+fastqc -o $OUTPUT_DIR --noextract -t 6 $INPUT_DIR/*.gz
