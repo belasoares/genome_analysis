f8608c52eb24639e947464c8d777f1f12d171554
Bela Soares de Almeida Dutta <bela@rackham1.uppmax.uu.se>
2024-08-25 18:49:53 +0200

    sbatch script for RNA trimming

--- analyses/01_preprocessing/RNA_qc/RNA_trimming/trim_paired_RNA.sh
+++ analyses/01_preprocessing/RNA_qc/RNA_trimming/trim_paired_RNA.sh
@@ -13,7 +13,7 @@ module load trimmomatic
 INPUT_DIR=/proj/uppmax2024-2-7/Genome_Analysis/2_Beganovic_2023/RNA_reads
 OUTPUT_DIR=/home/bela/genome_analysis/analyses/01_preprocessing/RNA_qc/RNA_trimming
 
-declare -a samples=("SRR24516460" "SRR24516461")
+declare -a samples=("SRR24516459" "SRR24516462" "SRR24516463" "SRR24516464")
 
 for sample_id in "${samples[@]}"
 do
@@ -27,8 +27,8 @@ do
     $OUTPUT_DIR/forward_unpaired_${base_f1} \
     $OUTPUT_DIR/reverse_paired_${base_f2} \
     $OUTPUT_DIR/reverse_unpaired_${base_f2} \
-    LEADING:20 \
-    TRAILING:20 \
+    LEADING:3 \
+    TRAILING:3 \
     SLIDINGWINDOW:4:20 \
-    MINLEN:100
+    MINLEN:20
 done
